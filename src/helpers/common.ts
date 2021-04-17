d8002f3032fe66862234fa30a8a944e04ae75d2b
eraldcafka <erald.c@scopicsoftware.com>
2021-04-17 14:07:36 +0200

    + Added 'common' module with some functions that will be used widely on routine operations

---
+++ src/helpers/common.ts
@@ -0,0 +1,21 @@
+// Pipe functionality
+export const pipe = <T>(...func: Array<Function>) => (data: T) => [...func].reduce((result, func) => func(result), data);
+
+// Grouping 'filter' related common functionalities
+export const filter = <T>(callback: (item: T, index: number) => boolean) => (collection: Array<T>) => collection.filter(callback);
+export const isTruthy = <T>(item: T) => !!item;
+
+// Concat functionality
+export const concat = <T>(...collections: Array<Array<T>>) => (initArray: Array<T>): Array<T> => initArray.concat(...collections);
+
+// Mapping functionality
+export const map = <T>(callback: (item: T, index: number) => any) => (collection: Array<T>): Array<any> => collection.map(callback);
+
+// Find functionality
+export const find = <T>(callback: (item: T, index: number) => any) => (collection: Array<T>): T => collection.find(callback);
+
+// Reduce functionality
+export const reduce = <T>(callback: (acc: any, item: T, index: number) => any, acc: any) => (collection: Array<T>) => collection.reduce(callback, acc);
+
+export const objectEntries = (data: object): Array<[string, any]> => Object.entries(data); 
+export const isNull = (value: any) => value === null;
